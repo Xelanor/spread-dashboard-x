54fc51fc2ec00e3b6c38dd43620afb7439b6539d
Xelanor <berkeozelsel@gmail.com>
2024-08-03 07:29:01 +0300

    Views

--- spread/urls.py
+++ spread/urls.py
@@ -2,4 +2,7 @@ from django.urls import path, include
 
 from spread import views
 
-urlpatterns = []
+urlpatterns = [
+    path("", views.spread_bots_data),
+    path("transactions", views.spread_transactions, name="spread_transactions"),
+]
--- spread/views.py
+++ spread/views.py
@@ -1,3 +1,73 @@
-from django.shortcuts import render
+import asyncio
+from datetime import datetime, timedelta
 
-# Create your views here.
+from rest_framework.response import Response
+from rest_framework.decorators import api_view
+from django.core.cache import cache
+from django.utils.timezone import now
+
+from dashboard.utils import async_maker_server_request
+from dashboard.models import Server
+
+
+@api_view(["GET"])
+def spread_bots_data(request):
+    if request.method == "GET":
+        url = "/spread"
+        servers = list(Server.objects.filter().values())
+
+        data = asyncio.run(async_maker_server_request(servers, "GET", url))
+
+        result = {"bots": []}
+        for server in data:
+            bots = data[server]["bots"]
+            for bot in bots:
+                bot_id = bot["settings"]["id"]
+                key = f"{server}_{bot_id}"
+                bot["settings"]["key"] = key
+                bot["settings"]["server"] = server
+
+                result["bots"].append(bot)
+
+        return Response(result)
+
+
+@api_view(["POST"])
+def spread_transactions(request):
+    body = request.data
+    bot_id = body["bot_id"]
+    server = body.get("server", None)
+
+    if server:
+        servers = list(Server.objects.filter(number=server).values())
+    else:
+        servers = list(Server.objects.filter().values())
+
+    url = "/spread/transactions"
+    data = asyncio.run(
+        async_maker_server_request(servers, "POST", url, {"bot_id": bot_id})
+    )
+
+    if bot_id == "all-transactions":
+        result = {
+            "ticker": "All Transactions",
+            "total_profit": 0,
+            "total_volume": 0,
+            "transactions": [],
+        }
+        for key in data:
+            result["transactions"].extend(data[key]["transactions"])
+            result["total_profit"] += data[key]["total_profit"]
+            result["total_volume"] += data[key]["total_volume"]
+
+        result["transactions"].sort(
+            key=lambda x: datetime.fromisoformat(
+                x["created_at"].replace("Z", "+00:00")
+            ),
+            reverse=True,
+        )
+
+        return Response(result)
+
+    else:
+        return Response(data[server])
